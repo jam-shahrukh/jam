4309e2a97cd6c6826845ad900c0e781d23bff69c
jam-shahrukh <81997538+jam-shahrukh@users.noreply.github.com>
2021-04-07 13:17:46 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -36,7 +36,7 @@ os.system('git pull')
 os.system('clear')
 logo = ('echo -e "\n\n    .S   .S_SSSs     .S_SsS_S.   \n   .SS  .SS~SSSSS   .SS~S*S~SS.  \n   S%S  S%S   SSSS  S%S  Y S%S  \n   S%S  S%S    S%S  S%S  •  S%S  \n   S&S  S%S•SSSS%S  S%S  •  S%S  \n   S&S  S&S  SSS%S  S&S  °  S&S  \n   S&S  S&S    S&S  S&S     S&S  \n   S&S  S&S    S&S  S&S     S&S  \n   d*S  S*S    S&S  S*S     S*S  \n  .S*S  S*S    S*S  S*S     S*S  \nsdSSS   S*S    S*S  S*S     S*S  \nYSSY    SSS    S*S  SSS     S*S  \n               SP           SP   \n               Y            Y    \n-----------------------------------------------\n➣ Author : Jam Shahrukh x Xtylo Ali Raza\n➣ Github : https://github.com/Blacklisted\n➣ Fb Page : https://m.facebook.com/Jam Shahrukh Official\n➣ Ref By : (Stylish Queen x Muskan Noor x Zahra Zohaib)\n➣ Ref By :     (Ahmed Gondal x ASAD x Janzada Khan) \n-----------------------------------------------" | lolcat')  
 
-def tlogin():
+def jam():
 	os.system('clear')
 	print logo
 	username = raw_input("[+] TOOL USERNAME: ")
@@ -47,7 +47,7 @@ def tlogin():
 	else:
 	    print '[!] Invalid Username.'
 	    time.sleep(1)
-	    tlogin()
+	    jam()
 	    
 	passw = raw_input("[+] TOOL PASSWORD: ")
 	if passw =="jam":
@@ -674,6 +674,6 @@ def a_s():
 
 
 if __name__ == '__main__':
-	tlogin():
+    jam():
         
 		
